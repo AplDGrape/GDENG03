25049561b8e496931492955a740f2e666111988f
AplDGrape <61457409+AplDGrape@users.noreply.github.com>
2025-07-01 17:12:44 +0800

    Update
    - Added UI mouse input handler to handle mouse events for blueprint/ui

--- DirectXGame/DirectXGame/Window.cpp
+++ DirectXGame/DirectXGame/Window.cpp
@@ -4,6 +4,10 @@
 
 #include "Window.h"
 #include "EngineTime.h"
+#include "imgui.h"
+
+//Declared for handling mouse and key events in IMGUI
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 Window* window = nullptr;
 
@@ -14,6 +18,9 @@ Window::Window()
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
+    if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam))
+        return true;
+
     switch (msg)
     {
     case WM_CREATE:
